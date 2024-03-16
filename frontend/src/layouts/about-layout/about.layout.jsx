07dee107d90cf9d0439ed6c4f2b7ba7b2028bbd2
Gabriel Palmar <palmarthc@gmail.com>
2024-03-16 17:26:26 -0400

    image codicon
    rounded border

--- frontend/src/layouts/about-layout/about.layout.jsx
+++ frontend/src/layouts/about-layout/about.layout.jsx
@@ -24,7 +24,7 @@ const AboutLayout = () => {
 			id: 1,
 			title: "¿Quiene somos?",
 			description: "En Transmivencia, creemos en la creación de un mundo más inclusivo y accesible para todos. Nuestra historia comenzó como una idea nacida durante un concurso de programación, la CODICON 2024, donde nos propusimos encontrar soluciones innovadoras para promover la inclusión y la diversidad en la tecnología. La palabra clave del concurso fue TRANSMISION.",
-			icon: <img src={Codicon} alt="" />
+			icon: <img src={Codicon} className="rounded-3xl" alt="" />
 		},
 		{
 			id: 2,
