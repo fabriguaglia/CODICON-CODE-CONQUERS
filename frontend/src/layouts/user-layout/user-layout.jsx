c8fcff5ab7f9951a5cf6dd6dd8ea42ba25c8956d
tcarloh <quezandoc@gmail.com>
2024-03-16 00:41:35 -0300

    issue:(core-design)
    Creo la base de la aplicacion, con manejo de layouts y componentes, los layoyts son vistas principales que contienen componentes dentro
    En services pondre la conexion con los datos

---
+++ frontend/src/layouts/user-layout/user-layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const UserLayout = () => {
+    return (
+        <div>
+            <header>
+                {/* Contenido del encabezado */}
+            </header>
+            <main>
+                {/* Contenido del cuerpo */}
+            </main>
+            <footer>
+                {/* Contenido del pie de página */}
+            </footer>
+        </div>
+    );
+};
+
+export default UserLayout;
