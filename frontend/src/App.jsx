c9c1eeae2e8d618bef6673e3e046568a0d41e5a8
Gabriel Palmar <palmarthc@gmail.com>
2024-03-16 08:55:44 -0400

    React router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 import MainLayout from './layouts/main-layout/main.layuout.jsx'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
 
 function App() {
 
-  return (
-    <>
-      <MainLayout/>
-    </>
-  )
+	return (
+		<HashRouter>
+			<Routes>
+				<Route path="/" element={<MainLayout />} />
+			</Routes>
+		</HashRouter>
+	)
 }
 
 export default App
